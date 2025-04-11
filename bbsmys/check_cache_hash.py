666a0ab6aada407d682d3b5c608a9039b80dc9b4
rua neko <rua_neko@163.com>
2025-04-11 19:39:11 +0800

    去除调试配置

--- bbsmys/check_cache_hash.py
+++ bbsmys/check_cache_hash.py
@@ -1,5 +1,8 @@
 from cache_utils import cache
+import hashlib
 
 # 计算缓存hash值，目前只考虑cookie和stoken，mid和stuid考虑为固定值
-str = f'{cache.get_value("cookie")}-{cache.get_value("stoken")}' 
-print(f'cookie_hash={hash(str)}')
+data = f'{cache.get_value("cookie")}-{cache.get_value("stoken")}' 
+# print(data)
+md5val = hashlib.md5(data.encode(encoding='UTF-8')).hexdigest()
+print(f'cookie_hash={md5val}')
